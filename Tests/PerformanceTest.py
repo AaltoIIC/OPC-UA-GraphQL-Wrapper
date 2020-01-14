388454dced66fe44b9cab49ba61cd1b9aa2a08f4
hietalj3 <jani.hietala@aalto.fi>
2020-01-14 21:07:49 +0200

    Write and OPC UA tests added.

--- Tests/PerformanceTest.py
+++ Tests/PerformanceTest.py
@@ -2,13 +2,13 @@ import requests
 import time
 from datetime import datetime
 from string import Template
-from opcua import Server
+from opcua import Client
 
 tests = [1, 5, 25, 100]
 queriesPerTest = 10
 
 GraphQL_API_URL = "http://127.0.0.1:8000/graphql/"
-OPC_UA_Endpoint = "opc.tcp://localhost:4840/prftestserver/"
+OPC_UA_Endpoint = "opc.tcp://localhost:4840/testserver/"
 
 queryAddServer = Template("""
     mutation {
@@ -49,6 +49,17 @@ def read_query_gen(n):
     query = query + "}"
     return query
 
+def write_query_gen(n):
+    query = "mutation { "
+    for i in range(1, n + 1):
+        node = "vn" + str(i) \
+            + ': node(server: "PrfTestServer", nodeId: "ns=2;i=' \
+            + str(i + 1)
+            + '", value: ' \
+            + i + ') { writeTime }'
+        query = query + node
+    query = query + "}"
+    return query
 
 def read_node_variable(session, query):
     # time.sleep(0.05)
@@ -60,7 +71,11 @@ def read_node_variable(session, query):
 
 
 def write_node_variable(session, query):
-    pass
+    start = time.time_ns()
+    response = session.post(GraphQL_API_URL, json={"query": query})
+    latency = round((time.time_ns() - start) / 1000000)
+    writeTime = response.json()["data"]["vn1"]["writeTime"]
+    return latency, round(writeTime / 1000000)
 
 
 with requests.Session() as session:
@@ -94,9 +109,73 @@ with requests.Session() as session:
                 "\nAverage read time: " + str(avgOpc)
             )
 
+        # Write tests
+        for test in tests:
+            query = write_query_gen(test)
+            tot = {"lat": 0, "opc": 0}
+            for i in range(queriesPerTest):
+                latency, writeTime = write_node_variable(session, query)
+                print(
+                    "Write " + str(test) + " latency: " + str(latency) +
+                    " Write Time: " + str(readTime)
+                )
+                tot["lat"] += latency
+                tot["opc"] += writeTime
+            avgLat = tot["lat"]/queriesPerTest
+            avgOpc = tot["opc"]/queriesPerTest
+            print(
+                "Write " + str(test) + " average latency: " + str(avgLat) +
+                "\nAverage write time: " + str(avgOpc)
+            )
+
     finally:
         # Remove set up test server from GraphQL API
         deleteQuery = queryDeleteServer.substitute({
             "name": "PrfTestServer"
         })
         session.post(GraphQL_API_URL, json={"query": deleteQuery})
+
+
+def read_node_variable_opcua(session, n):
+    params = ua.ReadParameters()
+    for i in range(1, n + 1):
+        rv = ua.ReadValueId()
+        rv.NodeId = ua.NodeId.from_string("ns=2;i=" + str(i + 1))
+        rv.AttributeId = ua.AttributeIds[Value]
+        params.NodesToRead.append(rv)
+    start = time.time_ns()
+    session.read(params)
+    latency = round((time.time_ns() - start) / 1000000)
+    return latency
+
+
+def write_node_variable_opcua(session, n):
+    params = ua.WriteParameters()
+    for i in range(1, n + 1):
+        rv = ua.WriteValue()
+        rv.NodeId = ua.NodeId.from_string("ns=2;i=" + str(i + 1))
+        rv.AttributeId = ua.AttributeIds[Value]
+        params.NodesToRead.append(rv)
+    start = time.time_ns()
+    session.read(params)
+    latency = round((time.time_ns() - start) / 1000000)
+    return latency
+
+
+with Client(OPC_UA_Endpoint) as session:
+
+    # OPC UA read tests
+    tot = 0
+    for test in tests:
+        latency = read_node_variable_opcua(session, test)
+        print("Read " + str(test) + " latency: " + str(latency))
+        tot += latency
+    print("Read " + str(test) + " average latency: " + str(tot))
+
+    # OPC UA write tests
+    tot = 0
+    for test in tests:
+        latency = write_node_variable_opcua(session, test)
+        print("Write " + str(test) + " latency: " + str(latency))
+        tot += latency
+    print("Write " + str(test) + " average latency: " + str(tot))
