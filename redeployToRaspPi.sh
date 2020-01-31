b657de7af4758e3bfad700b661700481b26ad5c3
Jani <jani.hietala@aalto.fi>
2020-01-31 13:13:32 +0200

    Docker updated to 3 workers. Redeploy IP changed.

--- redeployToRaspPi.sh
+++ redeployToRaspPi.sh
@@ -2,14 +2,6 @@
 echo ""
 echo "Run this file with ubuntu bash only from the project folder!"
 echo "This script copies all files from its folder that are not ignored in rsync-ignore.txt"
-echo "Starting redeployment"
 echo "Syncing files to server..."
 rsync -avz --exclude-from='rsync-ignore.txt' -e 'ssh' "$PWD" pi@192.168.0.77:/home/pi
-#echo "Connecting to server via ssh for service restart..."
-#ssh pi@192.168.0.77 '
-#docker container rm
-#echo "Reloading daemon..."
-#sudo systemctl daemon-reload
-#echo "Completed! Ending ssh connection..."
-#'
 echo "Finished"
